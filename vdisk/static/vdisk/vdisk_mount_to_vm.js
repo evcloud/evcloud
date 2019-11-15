c7977cbda3a5a8cd395e0cdb989f02fe76725a73
shun <869588058@qq.com>
2019-11-15 15:18:09 +0800

    虚拟机详情网页实现，js文件修改

--- vdisk/static/vdisk/vdisk_mount_to_vm.js
+++ vdisk/static/vdisk/vdisk_mount_to_vm.js
@@ -1,50 +1,6 @@
 ;(function () {
 
-    var VM_STATUS_CN = {
-        0: '故障0', //无状态
-        1: '运行',
-        2: '阻塞',
-        3: '暂停',
-        4: '关机',
-        5: '关机',
-        6: '崩溃',
-        7: '暂停',
-        8: '故障',  //libvirt预留状态码
-        9: '宿主机连接失败',  //宿主机连接失败
-        10: '云主机丢失'  //虚拟机丢失
-    };
-
-    var VM_STATUS_LABEL = {
-            0: 'default',
-            1: 'success',
-            2: 'info',
-            3: 'info',
-            4: 'info',
-            5: 'info',
-            6: 'danger',
-            7: 'info',
-            8: 'default',
-            9: 'danger',
-            10: 'default'
-    };
-
-    //API域名
-    let DOMAIN_NAME = get_domain_url();
     let API_VERSION = 'v3';
-
-    // 获取API域名
-    function get_api_domain_name(){
-        return DOMAIN_NAME;
-    }
-
-    // 构建带域名url
-    function build_absolute_url(url){
-        let domain = get_api_domain_name();
-        domain = domain.rightStrip('/');
-        if(!url.startsWith('/'))
-            url = '/' + url;
-        return domain + url;
-    }
 
     //
     // 页面刷新时执行
@@ -94,42 +50,10 @@
         update_vms_status(get_vm_list_uuid_array());
     });
 
-        // 虚拟机操作api构建
-    function build_vm_operations_api(vm_uuid){
-        let url = 'api/v3/vms/' + vm_uuid + '/operations/';
-        return build_absolute_url(url);
-    }
-
     // 虚拟机运行状态api构建
     function build_vm_status_api(vm_uuid){
-        let url = 'api/v3/vms/' + vm_uuid + '/status/';
+        let url = 'api/' + API_VERSION + '/vms/' + vm_uuid + '/status/';
         return build_absolute_url(url);
-    }
-
-    // 关机虚拟机
-    function shutdown_vm(vm_uuid){
-        let api = build_vm_operations_api(vm_uuid);
-        $.ajax({
-            url: api,
-            type: 'patch',
-            data: {
-                'op': 'shutdown',
-            },
-            success: function (data, status_text) {
-            },
-            error: function (xhr, msg, err) {
-                data = xhr.responseJSON;
-                msg = '虚拟机关机失败,';
-                if (data.hasOwnProperty('code_text')){
-                    msg = '虚拟机关机失败,' + data.code_text;
-                }
-                alert(msg);
-            },
-            complete:function (xhr, ts) {
-                let api = build_vm_status_api(vm_uuid);
-                get_vm_status(api, vm_uuid);
-            }
-        });
     }
 
     // 关机虚拟机点击事件
@@ -140,34 +64,12 @@
 		    return;
 
         let vm_uuid = $(this).attr('data-vm-uuid');
-        shutdown_vm(vm_uuid);
+        shutdown_vm_ajax(vm_uuid, null, function () {
+            let api = build_vm_status_api(vm_uuid);
+            get_vm_status(api, vm_uuid);
+        });
     });
 
-    // 强制断电虚拟机
-    function poweroff_vm(vm_uuid){
-        let api = build_vm_operations_api(vm_uuid);
-        $.ajax({
-            url: api,
-            type: 'patch',
-            data: {
-                'op': 'poweroff',
-            },
-            success: function (data, status_text) {
-            },
-            error: function (xhr, msg, err) {
-                data = xhr.responseJSON;
-                msg = '强制断电失败,';
-                if (data.hasOwnProperty('code_text')){
-                    msg = '强制断电失败,' + data.code_text;
-                }
-                alert(msg);
-            },
-            complete:function (xhr, ts) {
-                let api = build_vm_status_api(vm_uuid);
-                get_vm_status(api, vm_uuid);
-            }
-        });
-    }
 
     // 强制断电虚拟机点击事件
     $(".btn-vm-poweroff").click(function (e) {
@@ -177,7 +79,10 @@
 		    return;
 
         let vm_uuid = $(this).attr('data-vm-uuid');
-        poweroff_vm(vm_uuid);
+        poweroff_vm_ajax(vm_uuid, null, function () {
+            let api = build_vm_status_api(vm_uuid);
+            get_vm_status(api, vm_uuid);
+        });
     });
 
     // 挂载硬盘
