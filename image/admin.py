740f11bc68c740ab21d9eb817cad8a6fb38e9249
shun <869588058@qq.com>
2020-05-15 05:03:40 -0400

    一些admin相关细节修改

--- image/admin.py
+++ image/admin.py
@@ -1,26 +1,26 @@
 from django.contrib import admin
 
 from .models import VmXmlTemplate, Image, ImageType
-# Register your models here.
+
 
 @admin.register(VmXmlTemplate)
 class VmXmlTemplateAdmin(admin.ModelAdmin):
     list_display_links = ('id', 'name',)
-    list_display = ( 'id', 'name', 'desc')
+    list_display = ('id', 'name', 'desc')
     search_fields = ('name', 'desc')
 
 
 @admin.register(ImageType)
 class ImageTypeAdmin(admin.ModelAdmin):
     list_display_links = ('id', 'name',)
-    list_display = ( 'id', 'name')
+    list_display = ('id', 'name')
     search_fields = ('name',)
 
 
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
     list_display_links = ('id', 'name',)
-    list_display = ( 'id', 'name', 'version', 'tag', 'sys_type', 'type', 'base_image', 'snap', 'enable', 'xml_tpl', 'desc')
+    list_display = ('id', 'name', 'version', 'tag', 'sys_type', 'type', 'base_image', 'snap', 'enable', 'xml_tpl', 'desc')
     search_fields = ('name',)
     list_filter = ('type', 'enable', 'tag', 'sys_type')
     readonly_fields = ('snap',)
