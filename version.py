27158521270bef658b511b9015933d2760e7a2f0
liyuepeng <wojiaoliyuepeng@qq.com>
2022-07-21 16:33:16 +0800

    添加版本更新说明

--- version.py
+++ version.py
@@ -1,7 +1,7 @@
 from django.utils.version import get_version
 
 
-VERSION = (3, 1, 7, 'final', 1)     # 'alpha', 'beta', 'rc', 'final'
+VERSION = (3, 1, 8, 'final', 1)     # 'alpha', 'beta', 'rc', 'final'
 
 __version__ = get_version(VERSION)
 
