1f474f4240a9fef4d4b44791b727e99bf06c88a6
李跃鹏 <l908065729@qq.com>
2023-02-03 09:36:12 +0800

    增加VPN过期时间

--- vpn/admin.py
+++ vpn/admin.py
@@ -4,7 +4,7 @@ from .models import VPNAuth, VPNConfig
 
 @admin.register(VPNAuth)
 class VPNAuthAdmin(admin.ModelAdmin):
-    list_display = ['id', 'username', 'password', 'active', 'create_time', 'create_user', 'modified_time',
+    list_display = ['id', 'username', 'password', 'active', 'create_time', 'expired_time', 'create_user', 'modified_time',
                     'modified_user', 'remarks']
     list_display_links = ['id', 'username']
     search_fields = ['username', 'remarks']
