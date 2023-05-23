2548905c519045884c3f50a820e82d907fe68f22
wanghuang <wanghuangsuper@aliyun.com>
2023-05-23 17:28:05 +0800

    v3.1.10 版本信息

--- version.py
+++ version.py
@@ -5,7 +5,7 @@ import datetime
 from django.utils.version import get_version
 
 
-VERSION = (3, 1, 10, 'rc', 2)     # 'alpha', 'beta', 'rc', 'final'
+VERSION = (3, 1, 10, 'final', 0)     # 'alpha', 'beta', 'rc', 'final'
 
 
 def get_git_changeset():
