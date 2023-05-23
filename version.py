24890cdd61f9f45df8e9a678362dcff11820be4b
wanghuang <wanghuangsuper@aliyun.com>
2023-05-23 14:49:33 +0800

    版本日志内容格式修改

--- version.py
+++ version.py
@@ -15,46 +15,32 @@ def get_git_changeset():
         return None
     repo_dir = os.path.dirname(os.path.abspath(__file__))
 
-    # %(*refname) %(*authorname) %(*authoremail) %(*authordate) %(*subject)
-    get_tag = subprocess.run(
-        'git log --pretty=format:"%an || %ae || %at || %s || %d" -100',
+    git_log = subprocess.run(
+        "git for-each-ref --count=3 --sort='-taggerdate' "
+        "--format='%(refname:short) || %(taggerdate:format:%s) || %(*authorname) || %(*authoremail) || %(subject)'"
+        " refs/tags/*",
         capture_output=True,
         shell=True,
         cwd=repo_dir,
         text=True,
     )
+
     try:
-        cmd_output = get_tag.stdout
-        lines = cmd_output.split('\n')
-        # {tag: [[作者， 时间， 提交内容]]}
+        cmd_output = git_log.stdout
+        lines = cmd_output.split('\n')[0:3]
         tz = datetime.timezone.utc
-        git_tag_dict = {}
-        git_tag_info = []
-        tag_count = 1
-        tag = None
+        tags = []
         for line in lines:
-            if tag_count == 4:
-                break
+            tag = line.split('||')
+            if len(tag) == 5:
+                tag[1] = datetime.datetime.fromtimestamp(int(tag[1]), tz=tz)
+                tag[4] = tag[4].replace('*', '\n*')
+                tags.append(tag)
 
-            commit_info = line.split('||')
-            if commit_info[4].startswith('  (HEAD -> develop, tag:') or commit_info[4].startswith('  (HEAD -> master, tag:'):
-                com_tag = commit_info[4].split(',')[1]
-                commit_info[4] = '  (' + com_tag.replace(' ', '', 1) + ')'
-
-            if commit_info[4].startswith('  (tag:'):
-                if tag:
-                    git_tag_dict[tag] = git_tag_info
-                    tag_count += 1
-                tag = commit_info[4].replace('  (tag: ', '').replace(')', '')
-                git_tag_info = []
-            commit_info[2] = datetime.datetime.fromtimestamp(int(commit_info[2]), tz=tz)
-            commit_info[4] = commit_info[4].replace(' ', '')
-            git_tag_info.append(commit_info)
 
     except Exception:
         return None
-
-    return git_tag_dict
+    return tags
 
 
 __version__ = get_version(VERSION)
