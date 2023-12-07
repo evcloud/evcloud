e80a9a5c49582d1d035121974b74e1d7e5c052d7
18295015726 <wanghuangsuper@aliyun.com>
2023-12-07 10:51:51 +0800

    ipv6 配置文件不存在，导出报错到提示界面。

--- network/views.py
+++ network/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http.response import JsonResponse, HttpResponse
 
+from utils.errors import VmError
 from .models import Vlan, MacIP
 from .managers import VlanManager
 
@@ -57,10 +58,17 @@ def vlan_show(request):
         vlan = VlanManager().get_vlan_by_id(int(vlan_id), user=request.user)
         macips = VlanManager().get_macips_by_vlan(vlan)
         macips = macips.prefetch_related('ip_vm')  # 反向预查询（避免多次访问数据库）
-        if iptype == 'ipv4':
-            file_name, config_file = VlanManager().generate_config_file(vlan, macips)
-        else:
-            file_name, config_file = VlanManager().generate_config_file_v6(vlan, macips)
+        file_name = None
+        config_file = None
+        try:
+            if iptype == 'ipv4':
+                file_name, config_file = VlanManager().generate_config_file(vlan, macips)
+            else:
+                file_name, config_file = VlanManager().generate_config_file_v6(vlan, macips)
+        except Exception as e:
+            error = VmError(code=400, msg=str(e))
+            return error.render(request=request)
+
         # 设定文件头，这种设定可以让任意文件都能正确下载，而且已知文本文件不是本地打开
         response = HttpResponse(config_file, content_type='APPLICATION/OCTET-STREAM')
         response['Content-Disposition'] = 'attachment; filename=' + file_name   # 设定传输给客户端的文件名称
