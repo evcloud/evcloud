2c8a69fb4445adfe6fc603d9b297997e3eb401b6
wanghuang <wanghuangsuper@aliyun.com>
2024-04-11 19:39:45 +0800

    vpn 过期日期调整、按钮调整

--- vpn/forms.py
+++ vpn/forms.py
@@ -19,7 +19,7 @@ class VPNAddFrom(forms.Form):
                                widget=forms.TextInput(attrs={'class': 'form-control'}))
     password = forms.CharField(label='密码', min_length=6, max_length=64,
                                widget=forms.TextInput(attrs={'class': 'form-control'}))
-    expired_time = forms.DateTimeField(label='过期时间', widget=forms.DateTimeInput(format=('%Y-%m-%dT%H:%M'), attrs={'class': 'form-control', 'type': 'datetime-local'}))
+    # expired_time = forms.DateTimeField(label='过期时间', widget=forms.DateTimeInput(format=('%Y-%m-%dT%H:%M'), attrs={'class': 'form-control', 'type': 'datetime-local'}))
     active = forms.BooleanField(label='激活状态', required=False, help_text='选中（激活）可用，不选中不可用')
     remarks = forms.CharField(label='备注', required=False, max_length=255,
                               widget=forms.TextInput(attrs={'class': 'form-control'}))
