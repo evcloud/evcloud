3cb0406bf35c01def7c0830b4e7146af017d60e6
wanghuang <wanghuangsuper@aliyun.com>
2024-04-11 15:38:01 +0800

    增加 openvpn servcie文件

--- 00_script/config_systemctl.sh
+++ 00_script/config_systemctl.sh
@@ -7,6 +7,7 @@
 systemctl disable evcloud
 systemctl disable evcloud_vnc
 systemctl disable evcloud-vnc  # 后期删除
+systemctl disable openvpn.service
 
 rm /usr/lib/systemd/system/evcloud.service -f
 rm /usr/lib/systemd/system/evcloud-vnc.service -f
@@ -14,6 +15,7 @@ rm /usr/lib/systemd/system/evcloud_vnc.service -f
 
 ln -s /home/uwsgi/evcloud/00_script/evcloud.service /usr/lib/systemd/system/evcloud.service
 ln -s /home/uwsgi/evcloud/00_script/evcloud_vnc.service /usr/lib/systemd/system/evcloud_vnc.service
+ln -s /home/uwsgi/evcloud/00_script/openvpn.service /usr/lib/systemd/system/openvpn.service
 
 # 如果 软连接不生效 将 软连接命令注释掉 将下面两行内容打开 重新执行该文件
 #cp /home/uwsgi/evcloud/00_script/evcloud.service /usr/lib/systemd/system/ -f
@@ -22,3 +24,4 @@ ln -s /home/uwsgi/evcloud/00_script/evcloud_vnc.service /usr/lib/systemd/system/
 systemctl daemon-reload
 systemctl enable evcloud
 systemctl enable evcloud_vnc
+systemctl enable openvpn.service
