8b2fd05f08ad5e646e50c1cb2f7319106164ea67
wanghuang <wanghuangsuper@aliyun.com>
2024-04-11 15:42:10 +0800

    增加 openvpn servcie文件

--- 00_script/config_systemctl.sh
+++ 00_script/config_systemctl.sh
@@ -7,7 +7,7 @@
 systemctl disable evcloud
 systemctl disable evcloud_vnc
 systemctl disable evcloud-vnc  # 后期删除
-systemctl disable openvpn.service
+systemctl disable evcloud_openvpn.service
 
 rm /usr/lib/systemd/system/evcloud.service -f
 rm /usr/lib/systemd/system/evcloud-vnc.service -f
@@ -15,7 +15,7 @@ rm /usr/lib/systemd/system/evcloud_vnc.service -f
 
 ln -s /home/uwsgi/evcloud/00_script/evcloud.service /usr/lib/systemd/system/evcloud.service
 ln -s /home/uwsgi/evcloud/00_script/evcloud_vnc.service /usr/lib/systemd/system/evcloud_vnc.service
-ln -s /home/uwsgi/evcloud/00_script/openvpn.service /usr/lib/systemd/system/openvpn.service
+ln -s /home/uwsgi/evcloud/00_script/evcloud_openvpn.service /usr/lib/systemd/system/evcloud_openvpn.service
 
 # 如果 软连接不生效 将 软连接命令注释掉 将下面两行内容打开 重新执行该文件
 #cp /home/uwsgi/evcloud/00_script/evcloud.service /usr/lib/systemd/system/ -f
@@ -24,4 +24,4 @@ ln -s /home/uwsgi/evcloud/00_script/openvpn.service /usr/lib/systemd/system/open
 systemctl daemon-reload
 systemctl enable evcloud
 systemctl enable evcloud_vnc
-systemctl enable openvpn.service
+systemctl enable evcloud_openvpn.service
