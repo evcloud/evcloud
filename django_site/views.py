b99bbc3f77ce212de19ab9623af77e88540af60e
wanghuang <wanghuangsuper@aliyun.com>
2024-09-09 06:52:02 +0000

    前端页面优化：fetch请求修改为ajax，防止浏览器不兼容；

--- django_site/views.py
+++ django_site/views.py
@@ -6,29 +6,13 @@ from django.http import JsonResponse
 class SetTimezoneView(View):
 
     def post(self, request):
-        use_timezone = 'UTC'
-
-        # 获取原始请求体数据
-        raw_data = request.body
-
-        if not raw_data:
-            return JsonResponse({'error': 'body is null'}, status=400)
-
-        # 将字节数据解码为字符串，例如 UTF-8
-        try:
-            decoded_data = raw_data.decode('utf-8')
-        except UnicodeDecodeError:
-            return JsonResponse({'error': 'Unable to decode data'}, status=400)
-
-        try:
-            json_data = json.loads(decoded_data)
-        except json.JSONDecodeError:
-            return JsonResponse({'error': 'Invalid JSON'}, status=400)
-
-        if json_data['timezone']:
-            use_timezone = json_data['timezone']
+        default_timezone = 'UTC'
+        use_timezone = request.POST.get('timezone', None)
+        if use_timezone is None or use_timezone == default_timezone:
+            request.session['useTimeZone'] = default_timezone
+            return JsonResponse({'timezone': default_timezone}, status=200)
 
         # 保存用户的时区到 session 中
         request.session['useTimeZone'] = use_timezone
 
-        return JsonResponse({'status': 'success'})
+        return JsonResponse({'timezone': use_timezone}, status=200)
